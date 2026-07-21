d1c4c3c71fad24f5a4d7a38c76d81af5516eaa3a
Vivek Kumar <viveksingh599999@gmail.com>
2026-07-21 23:54:39 +0530

    Add initial files for ticket booking app

---
+++ week-5/ticketbookingapp/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import LoginControl from "./LoginControl";
+
+function App() {
+    return (
+        <div>
+            <LoginControl />
+        </div>
+    );
+}
+
+export default App;
---
+++ week-5/ticketbookingapp/src/UserGreeting.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function UserGreeting() {
+    return (
+        <h1>Welcome back</h1>
+    );
+}
+
+export default UserGreeting;
